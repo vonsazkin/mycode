53e98d5daa4686987a7db1b10ca6665d90a78892
vonsazkin <vonslistings@gmail.com>
2024-10-21 20:24:25 +0000

    React labs through lab 14

---
+++ advance-react/next.config.js
@@ -0,0 +1,27 @@
+// next.config.js
+module.exports = {
+  reactStrictMode: true, // Enable strict mode for React, helping to identify potential problems in the app
+  swcMinify: true, // Enable SWC-based minification for faster builds and smaller bundle sizes
+}
+// next.config.js
+const withPreact = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      if (!options.dev) {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          react: 'preact/compat', // Use Preact's React compatibility layer
+          'react-dom/test-utils': 'preact/test-utils', // Use Preact's test utilities
+          'react-dom': 'preact/compat', // Replace ReactDOM with Preact's version
+        };
+      }
+      return config;
+    },
+  });
+};
+
+module.exports = withPreact({
+  reactStrictMode: true, // Keep React strict mode enabled
+  swcMinify: true, // Keep SWC minification enabled
+});
+
