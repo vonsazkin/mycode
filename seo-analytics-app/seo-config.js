07adc77c3316b5ab472f729bdd0a40c97b038f36
vonsazkin <vonslistings@gmail.com>
2024-10-23 21:13:38 +0000

    React day 3 labs

---
+++ seo-analytics-app/seo-config.js
@@ -0,0 +1,16 @@
+export default {
+  title: 'Quantum Store - Advanced Gadgets for the Future',
+  description: 'Explore the latest in quantum and interstellar technology at Quantum Store.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://aux1-2fc23d95-bd74-4c9a-a077-f8e0566b96f6.live.alta3.com/',
+    site_name: 'Quantum Store',
+  },
+  twitter: {
+    handle: '@quantumstore',
+    site: '@quantumstore',
+    cardType: 'summary_large_image',
+  },
+};
+
